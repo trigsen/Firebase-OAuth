150caa6015054ea73c00b804234b6242f2d88737
Roman <romayeah@yandex.ru>
2020-09-17 15:54:01 +0300

    delete unnecessary comments

--- config-overrides.js
+++ config-overrides.js
@@ -2,13 +2,6 @@ const path = require('path');
 const {alias} = require('react-app-rewire-alias')
 
 module.exports = function (config, env) {
-    // config.resolve = {
-    //     ...config.resolve,
-    //     alias: {
-    //         ...config.resolve.alias,
-    //         '@': path.resolve(__dirname, './src'),
-    //     },
-    // }
     alias({
         '@': path.resolve(__dirname, './src')
     })(config);
